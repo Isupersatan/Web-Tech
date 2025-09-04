ebb52d9e6b4dba83f5aa87fe679dffdb4756014f
sayan pal <sayansuper330@gmail.com>
2025-09-04 12:14:00 +0530

    add edit and delete

--- Js/DOM/apps/ToDoList.js
+++ Js/DOM/apps/ToDoList.js
@@ -24,14 +24,40 @@ formTask.addEventListener('submit',function(e)
 })
 
 function displayTasks(tasks){
-    let display = document.getElementById("display-task");
+  const display = document.getElementById("display-task");
   display.innerHTML = ""; // clear old list
 
-  taskList.forEach((task) => {
+  if (taskList.length === 0) {
+    display.innerHTML = `
+      <li class="list-group-item text-center text-muted">
+        Add your task
+      </li>
+    `;
+    return;
+  }
+
+  taskList.forEach((task,i) => {
     display.innerHTML += `
       <li class="list-group-item list-group-item-primary mt-1">
         <span class="fw-bold">${task}</span>
+        <button class="btn btn-outline-danger btn-sm float-end" onclick="deleteTask(${i})">❌</button>
+        <button class="btn btn-outline-secondary btn-sm  float-end me-1" onclick="updateTask(${i})">Edit</button>
       </li>
     `;
   });
 };
+displayTasks(taskList);
+
+
+function deleteTask(id){
+  taskList.splice(id,1);
+  localStorage.setItem('tasks',JSON.stringify(taskList));
+  displayTasks(taskList);
+}
+
+function updateTask(id){
+  taskEle.value = taskList[id];
+  taskList.splice(id,1);
+  localStorage.setItem('tasks',JSON.stringify(taskList));
+  displayTasks(taskList);
+}
